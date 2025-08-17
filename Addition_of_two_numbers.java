d3f79a0316a5e3b877745a1c81803e5488fbf0ed
Kshitij <kshitijmittal2007@gmail.com>
2025-08-17 17:48:42 +0530

    Addition of 2 numbers

---
+++ Addition_of_two_numbers.java
@@ -0,0 +1,15 @@
+// Write a program to add two numbers and display the result (using scanner).
+
+import java.util.*;
+public class Addition_of_two_numbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        sc.close();
+    }
+}
