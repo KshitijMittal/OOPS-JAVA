fc233e36b41f833f5be2be6db8562b5dde9de5df
Kshitij <kshitijmittal2007@gmail.com>
2025-08-18 21:19:01 +0530

    Updated program

--- Print_Adress.java
+++ Print_Adress.java
@@ -1,18 +1,15 @@
 // Write a program to print your address
 
-//using single println statement
 public class Print_Adress {
     public static void main(String[] args) {
+        
+        //using single println statement
         System.out.println("Address: 123 Main Street, City, Country, Zip Code");
+
+        //using multiple println statements
+        System.out.println("Address:");
+        System.out.println("123 Main Street");
+        System.out.println("City, Country");
+        System.out.println("Zip Code: 123456");
     }
 }
-
-// //using multiple println statements
-// public class Address {
-//     public static void main(String[] args) {
-//         System.out.println("Address:");
-//         System.out.println("123 Main Street");
-//         System.out.println("City, Country");
-//         System.out.println("Zip Code");
-//     }
-// }
