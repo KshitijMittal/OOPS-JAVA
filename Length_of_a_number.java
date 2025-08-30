baf9b0ae174133f50da3b1df9889b904b5658b0e
Kshitij <kshitijmittal2007@gmail.com>
2025-08-30 10:10:01 +0530

    Create Length_of_a_number.java

---
+++ Length_of_a_number.java
@@ -0,0 +1,25 @@
+// Write a program to calculate the length of a number entered by the user.
+
+import java.util.*;
+public class Length_of_a_number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in) ;
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+        sc.close();
+        
+        int length = 0;
+        int temp = n;
+        
+        if (n == 0) {
+            length = 1; // Special case for 0
+        } else {
+            while (temp != 0) {
+                temp /= 10;
+                length++;
+            }
+        }
+        
+        System.out.println("The length of the number " + n + " is: " + length);
+    }
+}
