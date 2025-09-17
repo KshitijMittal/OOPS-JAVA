e86049857727d51cb776c95ed85f9494946767e2
Kshitij <kshitijmittal2007@gmail.com>
2025-09-17 22:34:13 +0530

    Create Super_Method.java

---
+++ Super_Method.java
@@ -0,0 +1,25 @@
+// Super Method in Java
+
+class Superclass{
+    public Superclass(){
+        System.out.println("First Constructor of Superclass");
+    }
+    public Superclass(int n){
+        System.out.println("Second Constructor of Superclass with value: " + n);
+    }
+}
+
+class Subclass extends Superclass{
+    public Subclass(){
+        System.out.println("First Constructor of Subclass");
+    }
+    public Subclass(int n){
+        System.out.println("Second Constructor of Subclass with value: " + n);
+    }
+}
+
+public class Super_Method {
+    public static void main(String[] args) {
+        Subclass obj1 = new Subclass();
+    }
+}
