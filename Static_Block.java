bb934ecf09981917428ba4d82caa1c9e7ee56910
Kshitij <kshitijmittal2007@gmail.com>
2025-09-19 11:34:11 +0530

    Created Static_Block.java

---
+++ Static_Block.java
@@ -0,0 +1,9 @@
+public class Static_Block {
+    // Static Block
+    static {
+        System.out.println("Static Block Initialized");
+    }
+    public static void main(String[] args) {
+        System.out.println("Main Method");
+    }
+}
