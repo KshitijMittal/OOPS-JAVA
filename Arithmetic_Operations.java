4f3c6f98390ebb3b8d7fc36963ed83c09528ac6b
Kshitij <kshitijmittal2007@gmail.com>
2025-11-24 05:40:19 +0530

    Update Arithmetic_Operations.java

--- Arithmetic_Operations.java
+++ Arithmetic_Operations.java
@@ -3,18 +3,28 @@
 import java.util.* ;
 public class Arithmetic_Operations {
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner (System.in);
         System.out.print("Enter the value of a: ");
         float a = sc.nextInt();
         System.out.print("Enter the value of b: ");
         float b = sc.nextInt();
         sc.close();
-        
+
         System.out.println("The addition of a and b is "+ (a+b));
         System.out.println("The subtraction of a and b is "+ (a-b));
         System.out.println("The multiplication of a and b is "+ (a*b));
         System.out.println("The division of a and b is "+ (a/b));
-        
+
     }
 }
+
+/*
+Output:
+Enter the value of a: 10
+Enter the value of b: 5
+The addition of a and b is 15.0
+The subtraction of a and b is 5.0
+The multiplication of a and b is 50.0
+The division of a and b is 2.0
+*/
