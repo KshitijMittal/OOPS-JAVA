b411f8dec9149559fea0dc5b84cc097ebb1b7462
Kshitij <kshitijmittal2007@gmail.com>
2025-12-11 10:22:18 +0530

    Update Meter_to_Feet.java

--- Meter_to_Feet.java
+++ Meter_to_Feet.java
@@ -3,13 +3,19 @@
 import java.util.* ;
 public class Meter_to_Feet {
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner (System.in);
         System.out.print("Enter the value in meters: ");
         float a = sc.nextFloat();
         sc.close();
-        
+
         System.out.print("Your value in feet is "+ (a * 3.2808399));
-        
+
     }
 }
+
+/*
+Output:
+Enter the value in meters: 10
+Your value in feet is 32.808399
+*/
